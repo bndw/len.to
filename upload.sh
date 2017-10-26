96c2dbfa615a0785e7bad8c883a5b5054cd53252
bndw <benjamindwoodward@gmail.com>
2017-10-25 21:55:14 -0700

    s3 upload script

---
+++ upload.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Uploads a file to the ginput s3 bucket
+
+if [[ "$#" -ne 1 ]] ; then
+  cat << EOF
+
+Uploads a file to the ginput s3 bucket
+
+Usage: $0 <path_to_image>
+EOF
+fi
+
+bucket=ginput
+filepath=$1
+filename=$(basename "$1")
+
+aws s3 cp --acl public-read "${filepath}" "s3://${bucket}/${filename}"
