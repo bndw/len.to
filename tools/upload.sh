484b4ff834b879ac42e9a9a3f1a1921458b6dbda
bndw <benjamindwoodward@gmail.com>
2019-04-08 13:04:07 -0700

    Strip EXIF data before uploading images

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Uploads an image to the len.to s3 bucket.
+# Uploads an image to the len.to s3 bucket. Depends on exiftool
 set -e
 
 if [[ "$#" -ne 1 ]] ; then
@@ -13,12 +13,22 @@ EOF
   exit 1
 fi
 
+if ! [[ -x "$(command -v exiftool)" ]] ; then
+  echo 'Error: must install exiftool first.' >&2
+  exit 1
+fi
+
 bucket=ginput
 filepath=$1
 filename=$(basename "$1")
 url="https://d17enza3bfujl8.cloudfront.net/${filename}"
 
+# Strip all metadata from the image
+exiftool -all= $1
+
+# Upload the image
 aws s3 cp --acl public-read "${filepath}" "s3://${bucket}/${filename}"
 
+# Copy the URL to the clipboard
 echo "${url}" | pbcopy
 echo "Image url copied to clipboard"
