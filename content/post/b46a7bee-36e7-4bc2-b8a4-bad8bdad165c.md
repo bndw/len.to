a4f69feeb9d299f5f97e029ff19078ec27568335
Ben Woodward <benjamindwoodward@gmail.com>
2019-06-06 16:42:08 -0700

    Location in tags (#16)
    * Script for adding location to frontmatter tags
    
    * Add location to tags
    
    * Cleanup tag links

--- content/post/b46a7bee-36e7-4bc2-b8a4-bad8bdad165c.md
+++ content/post/b46a7bee-36e7-4bc2-b8a4-bad8bdad165c.md
@@ -1,9 +1,13 @@
 ---
-date: "2016-09-05T18:41:12-07:00"
+title: b46a7bee-36e7-4bc2-b8a4-bad8bdad165c
+date: 2016-09-05T18:41:12-07:00
 draft: false
-title: "b46a7bee-36e7-4bc2-b8a4-bad8bdad165c"
-location: "Seattle, WA"
-tags: []
+location: Seattle, WA
+tags:
+- Seattle, WA
+
 ---
 
+
 ![](https://d17enza3bfujl8.cloudfront.net/20160815_01_01.jpg)
+
---
+++ tools/location-to-tags.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	ErrFileNotExist  = errors.New("file does not exist")
+	ErrYamlMalformed = errors.New("unable to parse yaml")
+)
+
+// Takes the Location and adds it to the head of the Tags for each post
+func main() {
+	const dir = "./content/post"
+
+	err := filepath.Walk(dir, func(path string, i os.FileInfo, err error) error {
+		filename := i.Name()
+		if i.IsDir() {
+			fmt.Printf("%s is dir, skipping\n", filename)
+			return nil
+		}
+
+		p, err := LoadPost(filepath.Join(dir, filename))
+		if err != nil {
+			return err
+		}
+
+		if p.MetadataEncoding == "yaml" {
+			p.Metadata.Tags = append([]string{p.Metadata.Location}, p.Metadata.Tags...)
+			if err := p.Write(path); err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("err: %s\n", err)
+	}
+
+}
+
+type Post struct {
+	*Metadata
+	MetadataEncoding string
+	Path             string
+	Content          []byte
+}
+
+type Metadata struct {
+	Title    string    `yaml:"title"`
+	Date     time.Time `yaml:"date"`
+	Draft    bool      `yaml:"draft"`
+	Location string    `yaml:"location"`
+	Tags     []string  `yaml:"tags"`
+}
+
+func (p *Post) Write(path string) error {
+	var body []byte
+
+	switch p.MetadataEncoding {
+	case "yaml":
+		m, err := yaml.Marshal(p.Metadata)
+		if err != nil {
+			return err
+		}
+		// The yaml frontmatter is surrounded by ---
+		body = append(body, []byte("---\n")...)
+		body = append(body, m...)
+		body = append(body, []byte("\n---")...)
+
+	default:
+		return fmt.Errorf("cannot write unknown MetadataEncoding %s", p.MetadataEncoding)
+	}
+
+	body = append(body, []byte("\n")...)
+	body = append(body, p.Content...)
+	body = append(body, []byte("\n")...)
+
+	return ioutil.WriteFile(filepath.Clean(path), body, 0644)
+}
+
+func LoadPost(path string) (*Post, error) {
+	b, err := ioutil.ReadFile(filepath.Clean(path))
+	if err != nil {
+		return nil, err
+	}
+
+	p := Post{
+		Metadata: &Metadata{},
+		Path:     path,
+	}
+
+	if bytes.Contains(b, []byte("---")) {
+		p.MetadataEncoding = "yaml"
+
+		parts := bytes.SplitN(b, []byte("---"), 3)
+		p.Content = parts[2]
+
+		if err := yaml.Unmarshal(parts[1], p.Metadata); err != nil {
+			return nil, ErrYamlMalformed
+		}
+	}
+
+	return &p, nil
+}
