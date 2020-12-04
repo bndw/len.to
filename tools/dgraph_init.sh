a85ad7ab966d582f5d5b522546eeaa6a2145e75c
bndw <benjamindwoodward@gmail.com>
2020-12-03 23:37:25 -0800

    Make dgraph addr configurable

--- tools/dgraph_init.sh
+++ tools/dgraph_init.sh
@@ -2,16 +2,15 @@
 
 set -ex
 
-DGRAPH=localhost:8080
+DGRAPH=${DGRAPH:-localhost:8080}
 IMG_RDF=dgraph_imgs.rdf
 SCHEMA_RDF=dgraph_schema.rdf
 
-# run dgraph
-docker run -d --name dgraph -p 8080:8080 -p 8000:8000 dgraph/standalone:v20.03.0
-sleep 5
-
 go run dgraph_schema.go > $SCHEMA_RDF
 go run dgraph_export_images.go > $IMG_RDF
+
+# drop existing data and schema
+curl -X POST "${DGRAPH}/alter" -d '{"drop_all": true}'
 
 # load schema
 curl -H "Content-Type: application/rdf" "$DGRAPH/alter?commitNow=true" -XPOST --data-binary @$SCHEMA_RDF
