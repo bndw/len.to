f7d366e62be46af68e41674a6c7f257df7c29822
bndw <ben@bdw.to>
2021-05-19 20:37:49 -0700

    Remove unused files

--- archetypes/post.md
+++
@@ -1,8 +0,0 @@
----
-title: "{{ replace .TranslationBaseName "-" " " | title }}"
-date: {{ .Date }}
-draft: false
-location: ""
-
----
-
--- tools/new_post.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-id=$(uuidgen | tr "[:upper:]" "[:lower:]")
-name=${id}.md
-prefix=post
-
-hugo new ${prefix}/${name}
-
-vi content/${prefix}/${name}
